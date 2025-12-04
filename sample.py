71d1539422b50ad484d6ea2b758bc38056516406
Rajesh chandran <rajeshchandranaws3@gmail.com>
2025-12-04 11:48:19 +0000

    test

---
+++ sample.py
@@ -0,0 +1,10 @@
+print('hello world')
+print(3*(2-1))
+num = 1
+num += 1
+print("num:", num)
+
+list = []
+list.append(("name", "version", "arn"))
+list.append(("name", "version", "arn"))
+print(list)
